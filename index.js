c13a09da1302d952a95e2447ce55d5b229c45fde
Melody <reticivis@gmail.com>
2024-08-14 01:21:07 -0500

    rewrite to use jekyll efficiently

--- index.js
+++ index.js
@@ -15,20 +15,23 @@ function getOffset(offset) {
     return "GMT" + (offset < 0 ? "+" : "-") + ("00" + Math.floor(o / 60)).slice(-2) + ":" + ("00" + (o % 60)).slice(-2);
 }
 
-// set header to timezone
-document.querySelector("#tzheader").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
-// add offset in mins
-let offset = new Date().getTimezoneOffset();
-document.querySelector("#tzbody").insertAdjacentHTML("beforeend",
-    `<h4>You are <span class="h3 font-weight-bold" id="offset">
-${offset === 0 ? "equal with" : (Math.abs(offset).toString() + " minutes " + (offset < 0 ? "ahead of" : "behind"))} GMT/UTC (${getOffset(offset)})</span></h4>`);
-// add different formats of timezone
-for (const zoneName of ['short', 'long', 'shortOffset', 'longOffset', 'shortGeneric', 'longGeneric']) {
-    try {
-        document.querySelector("#tzbody").insertAdjacentHTML("beforeend",
-            `<h3><span class="h4">${zoneName}: </span><b>${getTimeZone(zoneName)}</b></h3>`);
-    } catch (ex) {
-        console.error(ex);
+document.addEventListener("DOMContentLoaded", function () {
+    // set header to timezone
+    document.querySelector("#tzheader").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    // add offset in mins
+    let offset = new Date().getTimezoneOffset();
+    document.querySelector("#tzbody").insertAdjacentHTML("beforeend",
+        `<h4>You are <span class="h3 font-weight-bold" id="offset">
+    ${offset === 0 ? "equal with" : (Math.abs(offset).toString() + " minutes " + (offset < 0 ? "ahead of" : "behind"))} GMT/UTC (${getOffset(offset)})</span></h4>`);
+    // add different formats of timezone
+    for (const zoneName of ['short', 'long', 'shortOffset', 'longOffset', 'shortGeneric', 'longGeneric']) {
+        try {
+            document.querySelector("#tzbody").insertAdjacentHTML("beforeend",
+                `<h3><span class="h4">${zoneName}: </span><b>${getTimeZone(zoneName)}</b></h3>`);
+        } catch (ex) {
+            console.error(ex);
+        }
     }
-}
+
+});
 
